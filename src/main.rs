ea23473dd507f638da2cafac96634282d139cc1d
Denzyl Dick <denzyl@live.nl>
2021-10-16 14:19:54 +0200

    Display in different color.

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,12 @@ static TEST: &[u8] = b"Hey what are you looking at?";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let buffer = 0xb8000 as *mut u8;
+    let mut color = 0x0;
     for (i, &byte) in TEST.iter().enumerate() {
         unsafe {
+            color = color + 0x1;
             *buffer.offset(i as isize * 2) = byte;
-            *buffer.offset(i as isize * 2 + 1) = 0xb;
+            *buffer.offset(i as isize * 2 + 1) = color;
         }
     }
     loop {}
