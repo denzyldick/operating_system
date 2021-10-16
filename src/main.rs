8b6eaad7c9f6a71e3c40f612959b8b4e752572f3
Denzyl Dick <denzyl@live.nl>
2021-10-16 13:44:33 +0200

    Write a message to the address 0xb8000

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,16 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static TEST: &[u8] = b"Hey what are looking at?";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in TEST.iter().enumerate() {
+        unsafe {
+            *buffer.offset(i as isize * 2) = byte;
+            *buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
