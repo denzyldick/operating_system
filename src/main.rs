fb3e99ca557f00c74e3427ed4e5226e0d13e360f
Denzyl Dick <denzyl@live.nl>
2021-10-16 18:28:51 +0200

    Abstract way for writing some stuff to the VGA buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
-
 #![no_main]
+
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -9,18 +10,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static TEST: &[u8] = b"Hey what are you looking at?";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let buffer = 0xb8000 as *mut u8;
-    let mut color = 0x0;
-    for (i, &byte) in TEST.iter().enumerate() {
-        unsafe {
-            color = color + 0x1;
-            *buffer.offset(i as isize * 2) = byte;
-            *buffer.offset(i as isize * 2 + 1) = color;
-        }
-    }
+    vga_buffer::print();
     loop {}
 }
